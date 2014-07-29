e15966f7fd392e376b2d2b8e427584ed4bbffd86
Andrew M Westling & Christian Danielsen <student@epicodus.com>
2014-07-29 10:55:17 -0700

    add logic for any base up to 16

--- lib/binary.rb
+++ lib/binary.rb
@@ -16,3 +16,84 @@ def binary input
   output
 
 end
+
+
+def trinary input
+  str_arr = input.reverse!.split('')
+  tri_arr = []
+  output = 0
+
+  str_arr.each do |num|
+    tri_arr << num.to_i
+  end
+
+  0.upto(tri_arr.length-1) do |i|
+    if tri_arr[i] == 1
+      output += 3**i
+    elsif tri_arr[i] == 2
+      output += 2*3**i
+    end
+  end
+
+  output
+
+end
+
+def hex input
+  str_arr = input.reverse!.split('')
+  output = 0
+
+  0.upto(str_arr.length-1) do |i|
+    if str_arr[i] == "1"
+      output += 16**i
+    elsif str_arr[i] == "2"
+      output += 2*16**i
+    elsif str_arr[i] == "3"
+      output += 3*16**i
+    elsif str_arr[i] == "4"
+      output += 4*16**i
+    elsif str_arr[i] == "5"
+      output += 5*16**i
+    elsif str_arr[i] == "6"
+      output += 6*16**i
+    elsif str_arr[i] == "7"
+      output += 7*16**i
+    elsif str_arr[i] == "8"
+      output += 8*16**i
+    elsif str_arr[i] == "9"
+      output += 9*16**i
+    elsif str_arr[i] == "a"
+      output += 10*16**i
+    elsif str_arr[i] == "b"
+      output += 11*16**i
+    elsif str_arr[i] == "c"
+      output += 12*16**i
+    elsif str_arr[i] == "d"
+      output += 13*16**i
+    elsif str_arr[i] == "e"
+      output += 14*16**i
+    elsif str_arr[i] == "f"
+      output += 15*16**i
+    end
+  end
+
+  output
+
+end
+
+
+def any_base input, base
+  str_arr = input.reverse!.split('')
+  output = 0
+
+  0.upto(str_arr.length-1) do |i|
+    1.upto(base-1) do |j|
+      if str_arr[i] == j.to_s
+        output += j*base**i
+      end
+    end
+  end
+
+  output
+
+end
--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -5,5 +5,13 @@ describe("binary") do
   it("converts a given string of binary into its corresponding decimal value") do
     expect(binary('10')).to eq(2)
   end
+
+  it("converts a given string of ternary into its corresponding decimal value") do
+    expect(trinary('10')).to eq(3)
+  end
+
+  it("converts a given string of hexadecimal into its corresponding decimal value") do
+    expect(hex('10')).to eq(16)
+  end
 end
 
