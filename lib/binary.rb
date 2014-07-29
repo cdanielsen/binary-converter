b205aec0b72e7bb27144bdfd4b775a72f948de3c
Andrew M Westling & Christian Danielsen <student@epicodus.com>
2014-07-29 10:02:08 -0700

    refactor binary logic

--- lib/binary.rb
+++ lib/binary.rb
@@ -1,13 +1,11 @@
 def binary input
-  str_arr = input.split('')
+  str_arr = input.reverse!.split('')
   bin_arr = []
   output = 0
 
   str_arr.each do |num|
     bin_arr << num.to_i
   end
-
-  bin_arr.reverse!
 
   0.upto(bin_arr.length-1) do |i|
     if bin_arr[i] == 1
@@ -16,6 +14,5 @@ def binary input
   end
 
   output
-end
 
-puts binary('101')
+end
